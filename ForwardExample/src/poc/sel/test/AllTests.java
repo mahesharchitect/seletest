3b117505cc13f6c2ce8ecb5014b5d07d9b4d21ff
Mahesh.Chimanpure <maheshchimanpure@gmail.com>
2013-06-06 13:03:42 +0530

    For maven Junit with selenium support

---
+++ ForwardExample/src/poc/sel/test/AllTests.java
@@ -0,0 +1,14 @@
+package poc.sel.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ URLTest.class })
+public class AllTests {
+	public AllTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ForwardExample/src/poc/unint/LetsSee.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package poc.unint;
+
+/**
+ * @author Mahesh.Chimanpure
+ *
+ */
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class LetsSee {
+	String name,Id,address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
